259ed032af483db7d646623b8d3fa5500e72022f
Divinare <niemijoe@hotmail.com>
2014-02-08 18:45:07 +0200

    asdd

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,5 +6,5 @@
 # no regular words or you'll be exposed to dictionary attacks.
 Ratebeer::Application.config.secret_token = '46cd65efd3570f4941e00e591f85648c8593fb3a9d7e165ea8c01c597250bbb3cc296733323a98f57b44213bb722d1ad76cc6f8ce09eff337f507fc6a3dbdf6d'
 
-Susanoo::Application.config.secret_token = 'dL2dCIRt4Frl9JErtFfbVNTjGgHV5/enenWNPIUP5A4='
-Susanoo::Application.config.secret_key_base = '872501e756ba3c3f81c1ec5503f70cec802cdb8582cb755104c350addbf0ea206cc784f1d0d34de975ec142c296c675051373ce4957df00e8c77344683f92981' # this needs to be added
+#Susanoo::Application.config.secret_token = 'dL2dCIRt4Frl9JErtFfbVNTjGgHV5/enenWNPIUP5A4='
+#Susanoo::Application.config.secret_key_base = '872501e756ba3c3f81c1ec5503f70cec802cdb8582cb755104c350addbf0ea206cc784f1d0d34de975ec142c296c675051373ce4957df00e8c77344683f92981' # this needs to be added
