ffd0423598ecb0e77a34cf498eecde07a0da8fd1
Divinare <niemijoe@hotmail.com>
2014-02-18 20:39:12 +0200

    saf

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+u = User.create :username => "Div", :password_digest => "haha"
+
 b1 = Brewery.create :name => "Koff", :year => 1897
 b2 = Brewery.create :name => "Malmgard", :year => 2001
 b3 = Brewery.create :name => "Weihenstephaner", :year => 1042
